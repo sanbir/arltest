e3cadc66d4e6f94b64e2d86039f06693dae77e4c
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-06-04 17:12:12 +0600

    useless stuff

--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -10,6 +10,38 @@ namespace TestTask
     {
         static void Main(string[] args)
         {
+            int x = 6;
+            int n = 1;
+
+            Guid guid;
+            bool isDone = false;
+            List<Guid> list = new List<Guid>();
+            string template = "00000000";
+
+            while (list.Count < 100)
+            {
+                guid = Guid.NewGuid();
+                string s = guid.ToString().Replace("-", "");
+
+
+                //if (s.Contains(template))
+                {
+                    list.Add(guid);
+                    Console.WriteLine(s);
+                }
+                if (list.Count == n) break;
+            }
+
+            //for (int i = 0; i < 1000; i++)
+            //{
+            //    guid = Guid.NewGuid();
+            //    string s = guid.ToString();
+            //    Console.WriteLine(s);
+            //}
+
+
+            //Console.WriteLine(guid);
+            Console.ReadLine();
         }
     }
 }
