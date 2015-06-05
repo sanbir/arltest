d788920ebbdc0020ca339f7bbbefdaf5045c137b
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-06-05 10:13:43 +0600

    good algorythm

--- TestTask/TestTask/Program.cs
+++ TestTask/TestTask/Program.cs
@@ -13,24 +13,24 @@ namespace TestTask
             int x = 6;
             int n = 1;
 
-            Guid guid;
-            bool isDone = false;
-            List<Guid> list = new List<Guid>();
-            string template = "00000000";
+            //Guid guid = new Guid();
+            //bool isDone = false;
+            //List<Guid> list = new List<Guid>();
+            //string template = "00000000";
 
-            while (list.Count < 100)
-            {
-                guid = Guid.NewGuid();
-                string s = guid.ToString().Replace("-", "");
+            //while (list.Count < 100)
+            //{
+            //    guid = Guid.NewGuid();
+            //    string s = guid.ToString().Replace("-", "");
 
 
-                //if (s.Contains(template))
-                {
-                    list.Add(guid);
-                    Console.WriteLine(s);
-                }
-                if (list.Count == n) break;
-            }
+            //    //if (s.Contains(template))
+            //    {
+            //        list.Add(guid);
+            //        Console.WriteLine(s);
+            //    }
+            //    if (list.Count == n) break;
+            //}
 
             //for (int i = 0; i < 1000; i++)
             //{
@@ -39,9 +39,48 @@ namespace TestTask
             //    Console.WriteLine(s);
             //}
 
+            Guid guid = new Guid("f8306441-f248-4dfa-96c0-d3000000cb75");
+
+            byte digit = 0;
+            int requiredNumberOfZeroes = x;
+            int actualNumberOfZeroes = 0;
+
+            byte[] bytes = guid.ToByteArray();
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                uint temp = bytes[i];
+
+                if (temp >> 4 == 0)
+                {
+                    ++actualNumberOfZeroes;
+                    if (actualNumberOfZeroes == requiredNumberOfZeroes)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    actualNumberOfZeroes = 0;
+                }
+
+                if (temp << 28 == 0)
+                {
+                    ++actualNumberOfZeroes;
+                    if (actualNumberOfZeroes == requiredNumberOfZeroes)
+                    {
+                        break;
+                    }
+                }
+                else
+                {
+                    actualNumberOfZeroes = 0;
+                }
+            }
+
 
             //Console.WriteLine(guid);
             Console.ReadLine();
         }
+
     }
 }
