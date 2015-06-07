df3e3c16b2d65d4a6918e703b4aa98d319ddeb1c
Alexander Biryukov <biryukovao@edu.stu.ru>
2015-06-08 03:06:03 +0600

    2nd good

---
+++ BiryukovTest/BusinessLayer.BusinessTasks/Second/SecondTaskGuids.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Contracts.Second;
+
+namespace BusinessLayer.BusinessTasks.Second
+{
+    public class SecondTaskGuids : ISecondTask<Guid, string>
+    {
+        public ISecondTaskResult<string> Calculate(int numberOfGuids)
+        {
+            IList<Guid> guids = new List<Guid>();
+
+            for (int i = 0; i < numberOfGuids; i++)
+            {
+                guids.Add(Guid.NewGuid());
+            }
+
+            return Calculate(new SecondTaskArgumentsGuids { Items = guids });
+        }
+
+        public ISecondTaskResult<string> Calculate(ISecondTaskArguments<Guid> arguments)
+        {
+            var result = new SecondTaskResultStrings
+            {
+                Items = arguments.Items.Select(guid => guid.ToString()),
+                MaxCommonStrings = arguments.Items.Select(guid => guid.ToString("N")).ToList().GetLongestCommonSubstrings()
+            };
+
+            return result;
+        }
+    }
+}
---
+++ BiryukovTest/BusinessLayer.BusinessTasks/Second/SecondTaskResultStrings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BusinessLayer.Contracts.Second;
+
+namespace BusinessLayer.BusinessTasks.Second
+{
+    public class SecondTaskResultStrings : ISecondTaskResult<string>
+    {
+        public IEnumerable<string> Items { get; set; }
+        public IEnumerable<string> MaxCommonStrings { get; set; }
+    }
+}
